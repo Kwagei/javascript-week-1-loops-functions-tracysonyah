e3ac83c7af17cab8818b104d6fb1cd9d91fda2a8
Tracy Sonyah <sonyahtracy@gmail.com>
2023-06-08 21:25:57 +0000

    not completely done yet

--- problem3.js
+++ problem3.js
@@ -2,11 +2,11 @@
 //the highest number in the array and prints the average of the numbers.
 let arr = [1, 5, 30, 56, 3, 48, 67, 44, 7, 32];
 
-function highest_average(nums) {
-    highest_num = nums[0];
-    for(let i = 1; i < nums.length; i++) {
-        if(highest_num < nums[i]) {
-            highest_num = nums[i];
+function highest_average(ints) {
+    highest_num = ints[0];
+    for(let i = 1; i < ints.length; i++) {
+        if(highest_num < ints[i]) {
+            highest_num = ints[i];
         }
     }
     console.log(highest_num);
