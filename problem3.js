d6b358d9d172be298e55653989bd907b454348e7
Tracy Sonyah <sonyahtracy@gmail.com>
2023-06-08 23:53:21 +0000

    i think i'm done

--- problem3.js
+++ problem3.js
@@ -4,6 +4,7 @@ let arr = [1, 5, 30, 56, 3, 48, 67, 44, 7, 32];
 
 function highest_average(ints) {
     highest_num = ints[0];
+    
     for(let i = 1; i < ints.length; i++) {
         if(highest_num < ints[i]) {
             highest_num = ints[i];
--- problem5.js
+++ problem5.js
@@ -1,7 +1,13 @@
 //Two numbers are given write a function that prints the gratest common divisor of those numbers.
-var num1 = 22;
-var num2 = 33;
+// var num1 = 22;
+// var num2 = 33;
 
-function gcd() {
-    
+function gcd(a, b) {
+    while (b !== 0) {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    console.log(a)
 }
+gcd(2,4);
